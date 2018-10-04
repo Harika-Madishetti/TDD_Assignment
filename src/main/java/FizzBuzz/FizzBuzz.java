d582b6476709942fccaeea2c23104b4bc500ba86
Harika-Raj <r121662@rguktrkv.ac.in>
2018-10-04 20:18:02 +0530

    Returns buzz if the number is divisible by 5

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -1,11 +1,16 @@
 package FizzBuzz;
 
 public class FizzBuzz {
+    private static final String FIZZ = "FIZZ";
+    private static final int THREE = 3;
+    private static final String BLANK = "";
 
     public String print(int number) {
-        if(number % 3 ==0){
+        if(number % THREE == 0){
             return "FIZZ";
+        }else if(number % 5 ==0){
+            return  "BUZZ";
         }
-        return "" + number;
+        return  BLANK + number;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,27 +1,31 @@
 import FizzBuzz.FizzBuzz;
 import org.junit.Test;
-
 import static junit.framework.TestCase.assertEquals;
 
 public class FizzBuzzTest {
+     private FizzBuzz fizzbuzz = new FizzBuzz();
     @Test
     public void print_the_number_1(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertEquals("1",fizzbuzz.print(1));
     }
     @Test
     public void print_same_number_as_given(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals("20",fizzbuzz.print(20));
+        assertEquals("2",fizzbuzz.print(2));
     }
     @Test
     public void print_fizz_if_the_number_3(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertEquals("FIZZ",fizzbuzz.print(3));
     }
     @Test
     public void print_fizz_if_the_number_is_divisible_by_3(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertEquals("FIZZ",fizzbuzz.print(6));
+    }
+    @Test
+    public void print_buzz_if_the_number_is_5(){
+        assertEquals("BUZZ",fizzbuzz.print(5));
+    }
+    @Test
+    public void print_buzz_if_the_number_is_divisible_by_5(){
+        assertEquals("BUZZ",fizzbuzz.print(20));
     }
 }
