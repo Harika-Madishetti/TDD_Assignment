ea7a293cbb9b41e535e3bcec5d57511faed74991
Harika-Raj <r121662@rguktrkv.ac.in>
2018-10-04 22:14:09 +0530

    check wheather the given number is prime or not

---
+++ src/main/java/PrimeNumber/Primenumber.java
@@ -0,0 +1,11 @@
+package PrimeNumber;
+
+public class Primenumber {
+
+    public boolean check(int i) {
+        if(i % 2 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/test/java/PrimeNumberTest.java
+++ src/test/java/PrimeNumberTest.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import PrimeNumber.Primenumber;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PrimeNumberTest {
+    @Test
+    public void checkWeatherTwoIsPrime(){
+        Primenumber primenumber = new Primenumber();
+        assertTrue(primenumber.check(11));
+    }
+    @Test
+    public void checkWeatherFourIsPrime(){
+        Primenumber primenumber = new Primenumber();
+        assertFalse(primenumber.check(4));
+    }
+    @Test
+    public void checkWeatherFiveIsPrime(){
+        Primenumber primenumber = new Primenumber();
+        assertFalse(primenumber.check(8));
+    }
 }
