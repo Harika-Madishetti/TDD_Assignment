97f118c689dc1cebef7654c77a0b2c673c47268b
Harika-Raj <r121662@rguktrkv.ac.in>
2018-10-06 16:15:42 +0530

    Refactor code in fibonacci series in assignment2

--- src/main/java/Assignment2/FibonacciSeries.java
+++ src/main/java/Assignment2/FibonacciSeries.java
@@ -1,13 +1,13 @@
 package Assignment2;
 
 public class FibonacciSeries {
-    private static final int TWO = 2;
-    private static final int ONE = 1;
+    private static final int  COUNT= 2;
+    private static final int FIRST_NUMBER_IN_SERIES = 1;
 
     public int get(int index) {
-        if(index < TWO) {
-            return ONE;
+        if(index < COUNT) {
+            return FIRST_NUMBER_IN_SERIES;
         }
-        return get(index - ONE) + get(index - TWO);
+        return get(index - 1) + get(index - 2);
     }
 }
--- src/test/java/Assignment2/FibonacciSeriesTest.java
+++ src/test/java/Assignment2/FibonacciSeriesTest.java
@@ -1,6 +1,6 @@
 package Assignment2;
 
-import Assignment2.FibonacciSeries;
+
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
