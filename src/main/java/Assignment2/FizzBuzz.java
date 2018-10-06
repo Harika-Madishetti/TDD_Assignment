41ed3e88369feb93885aa456e0613bfc755ff49e
Harika-Raj <r121662@rguktrkv.ac.in>
2018-10-06 16:43:20 +0530

    Refactor the code in fizzbuzz program in assignment2

--- src/main/java/Assignment2/FizzBuzz.java
+++ src/main/java/Assignment2/FizzBuzz.java
@@ -5,13 +5,15 @@ public class FizzBuzz {
     private static final int FIVE = 5;
     private static final int ZERO = 0;
     private static final String BLANK = "";
-
     public String print(int number) {
-        if(number % THREE == ZERO && number % FIVE == ZERO) {
+        final boolean isNumberDivisibleByFifteen = number % THREE == 0 && number % 5 ==0;
+        final boolean isNumberDivisibleByThree = number % THREE == 0 ;
+        final boolean isNumberDivisibleByFive = number % FIVE == 0 ;
+        if(isNumberDivisibleByFifteen) {
             return "FIZZBUZZ";
-        }else if(number % THREE == ZERO){
+        }else if(isNumberDivisibleByThree){
             return "FIZZ";
-        }else if(number % FIVE == ZERO){
+        }else if(isNumberDivisibleByFive){
             return  "BUZZ";
         }
         return  BLANK + number;
