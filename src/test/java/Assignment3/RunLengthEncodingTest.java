e37bd2ea4656a9eac7aa30cb0b6b440ea1beb954
Harika-Raj <r121662@rguktrkv.ac.in>
2018-10-17 10:06:13 +0530

    Add intial testcase

--- src/test/java/Assignment3/RunLengthEncodingTest.java
+++ src/test/java/Assignment3/RunLengthEncodingTest.java
@@ -1,6 +1,10 @@
 package Assignment3;
-
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
 
 public class RunLengthEncodingTest {
-
+    @Test
+    public void encode_a(){
+        assertEquals("a1", new RunLength("a").encode());
+    }
 }
