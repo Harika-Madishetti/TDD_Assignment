7caebfd69bc2400a261e665c736bad38c07842c0
Harika-Raj <r121662@rguktrkv.ac.in>
2018-11-06 23:32:56 +0530

    Refactor the code

--- src/main/java/Assignment5/ProducerConsumerDemo.java
+++ src/main/java/Assignment5/ProducerConsumerDemo.java
@@ -4,7 +4,6 @@ public class ProducerConsumerDemo {
     public static void main(String[] args) {
 
         SharedResource sharedResource = new SharedResource();
-
         Thread producer = new Thread(new Producer(sharedResource));
         producer.start();
         for (int iterator = 0; iterator <= 5; iterator++) {
